ba7ebcff0901ac0f651dbf36634d44cdbaf426f2
andreas.kaeberlein <andreas.kaeberlein@web.de>
2017-03-26 21:14:57 +0200

    resistorSubstitution.m
    -> begin to add command line parser
    -> SRC1: https://de.mathworks.com/help/matlab/ref/inputparser-class.html
    -> SRC2: https://www.gnu.org/software/octave/doc/interpreter/Variable_002dlength-Argument-Lists.html

--- etools/resistorSubstitution.m
+++ etools/resistorSubstitution.m
@@ -1,11 +1,12 @@
 % ***********************************************************************
-%  License          : LGPL v3
+%  License          : LGPLv3
 %
 %  Author           : Andreas Kaeberlein
 %  eMail            : andreas.kaeberlein@web.de
 %
 %  File             : resistorSubstitution.m
 %  Description      : replaces given resistor value by E-series combination
+%  Octave           : 4.0
 %
 %  Sources          : none
 %
@@ -14,7 +15,7 @@
 
 
 
-function subsResistors = resistorSubstitution(value, rele, eseries)
+function subsResistors = resistorSubstitution(value, varargin)
 %%
 %%  Usage
 %%  =====
@@ -24,3 +25,24 @@ function subsResistors = resistorSubstitution(value, rele, eseries)
 %%  eseries:        integer         -> used E series for searching                              [ 96 ]
 %%  listings:       string array    -> found files
 %%
+
+
+% parse input
+% SRC: https://www.gnu.org/software/octave/doc/interpreter/Variable_002dlength-Argument-Lists.html
+%
+p                   = inputParser;  % init input parsewr
+defaultResRelError  = 1e-2;         % default relative resistor mismatch is 1%
+defaultEseries      = 24;           % E24 is the default series
+defaultResRange     = [1 10e6];     % Min/Max of avialable Resistors for substitution
+
+%addOptional(p, 'ResistorRange', defaultResRange, @ismatrix);    % optional parameter
+addRequired(p,'width',@isnumeric);
+
+
+parse(p, value, varargin{:});       % parse arguments
+%
+
+
+
+
+disp(num2str(defaultResRange))
