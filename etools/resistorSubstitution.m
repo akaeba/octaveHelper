e28cd2daefceff54ab414855c95e342afe02ec99
andreas.kaeberlein <andreas.kaeberlein@web.de>
2017-03-27 12:27:04 +0200

    resistorSubstitution.m
    -> finished function parameter list

--- etools/resistorSubstitution.m
+++ etools/resistorSubstitution.m
@@ -21,9 +21,9 @@ function subsResistors = resistorSubstitution(varargin)
 %%  =====
 %%
 %%  value:          double          -> resistor value to replace
-%%  tolerance:      double          -> residualer relative replacement error (rele = dR/R)      [ 1% ]
-%%  eseries:        integer         -> used E series for searching                              [ 96 ]
-%%  listings:       string array    -> found files
+%%  tolerance:      double          -> residualer relative deviation (tolerance = dR/R)     [ 1% ]
+%%  eseries:        string          -> used E series for searching                          [ E24 ]
+%%  resistors:      double array    -> collection of resistors or min/max value for eseries [ 1 10e6 ]
 %%
 
 
@@ -35,33 +35,22 @@ p 				= inputParser();            % create object
 p.FunctionName	= 'resistorSubstitution';   % set function name
 
 p.addRequired('value', @isnumeric);             % mandatory argument
-p.addOptional('tolerance', 0.01, @isnumeric);   % tolerance with 0.01 default
-p.addOptional('eseries', 24, @isnumeric);       % Eseries of used resistors,        Info: https://de.wikipedia.org/wiki/E-Reihe
 
-
-
+p.addOptional('tolerance', 0.01, @isnumeric);                                                                       % tolerance with 0.01 default
+p.addOptional('eseries', 'E24', @(x) any (strcmp (x, {'E3', 'E6', 'E12', 'E24', 'E48', 'E96', 'E192', 'None'})));   % Info: https://de.wikipedia.org/wiki/E-Reihe
+p.addOptional ('resistors', [1 10e6], @isvector);                                                                   % Min/max of resistor for E-Series generation, or collection of resistors
 
 p.parse(varargin{:});   % Run created parser on inputs
-
-p.Results.value
-p.Results.tolerance
-p.Results.eseries
-
-
-
-%p                   = inputParser;  % init input parsewr
-%defaultResRelError  = 1e-2;         % default relative resistor mismatch is 1%
-%defaultEseries      = 24;           % E24 is the default series
-%defaultResRange     = [1 10e6];     % Min/Max of avialable Resistors for substitution
-
-%addOptional(p, 'ResistorRange', defaultResRange, @ismatrix);    % optional parameter
-%addRequired(p,'width',@isnumeric);
-
-
-%parse(p, value, varargin{:});       % parse arguments
 %
 
 
 
 
-%disp(num2str(defaultResRange))
+
+
+p.Results.value
+p.Results.tolerance
+p.Results.eseries
+p.Results.resistors
+
+
